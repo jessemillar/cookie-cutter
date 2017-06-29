bb1641591dbe16538ac60c88468ac4b7c531c7ca
Jesse Millar <Jesse.Millar@walmart.com>
2017-06-29 16:14:00 -0500

    README update and variable naming changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# cookie-cutter
-A wallpaper/avatar maker utilizing ImageMagick's Duff-Porter Alpha Composition Methods
+## Usage
+```
+./cookie-cutter.sh mask.png image.jpg result.png [resultWidth (optional)] [resultHeight (optional)] [resultBackgroundColor (optional)]
+```
+
+> The script will find the image's average color and apply that to the background if no background color is supplied. If no width, height, or color is passed, the result with have a transparent background and the same dimensions as the mask.
--- cookie-cutter.sh
+++ cookie-cutter.sh
@@ -15,7 +15,7 @@ IMAGE_HEIGHT="$(convert $IMAGE -ping -format "%h" info:)"
 
 # Variables for temporary names
 TEMP_IMAGE="temp_image.png"
-TEMP_BOLT="temp_bolt.png"
+TEMP_MASK="temp_mask.png"
 TEMP_WALLPAPER="temp_wallpaper.png"
 
 # Check for image size issues
@@ -42,19 +42,19 @@ fi
 echo "Info: Scaling image"
 convert $IMAGE -geometry x${MASK_HEIGHT} $TEMP_IMAGE # Scale the image to better match the mask height
 echo "Info: Applying mask to image"
-composite -compose Dst_In -gravity center $MASK $TEMP_IMAGE -alpha Set $TEMP_BOLT # Apply the mask
+composite -compose Dst_In -gravity center $MASK $TEMP_IMAGE -alpha Set $TEMP_MASK # Apply the mask
 echo "Info: Cropping resulting image to mask size"
-convert $TEMP_BOLT -gravity center -crop ${MASK_WIDTH}x${MASK_HEIGHT}+0+0 +repage $TEMP_BOLT # Crop the final image to be the size of the mask
+convert $TEMP_MASK -gravity center -crop ${MASK_WIDTH}x${MASK_HEIGHT}+0+0 +repage $TEMP_MASK # Crop the final image to be the size of the mask
 
 # Build the final wallpaper if needed
 if [ -n "$WALLPAPER_WIDTH" ] && [ -n "$WALLPAPER_HEIGHT" ]
 then
 	echo "Info: Creating wallpaper"
-	composite -gravity center $TEMP_BOLT $TEMP_WALLPAPER $RESULT # Overlay the created bolt on the wallpaper
-	rm $TEMP_BOLT
+	composite -gravity center $TEMP_MASK $TEMP_WALLPAPER $RESULT # Overlay the created mask on the wallpaper
+	rm $TEMP_MASK
 	rm $TEMP_WALLPAPER
 else
-	mv $TEMP_BOLT $RESULT
+	mv $TEMP_MASK $RESULT
 fi
 
 echo "Info: Removing temporary files"
